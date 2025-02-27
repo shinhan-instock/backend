4cb9340ead27e734726087154a1177e3c6853e44
hjh <98207658+jjjuhoon@users.noreply.github.com>
2025-02-27 09:52:28 +0900

    feat(#21): exceptionAdvice-오류-해결

--- community-module/src/main/java/com/pda/community_module/CommunityModuleApplication.java
+++ community-module/src/main/java/com/pda/community_module/CommunityModuleApplication.java
@@ -3,11 +3,13 @@ package com.pda.community_module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
+@ComponentScan(basePackages = {"com.pda.community_module", "com.pda.core_module"})
 public class CommunityModuleApplication {
 
 	public static void main(String[] args) {
--- piggyBank-module/src/main/java/com/pda/piggyBank_module/PiggyBankModuleApplication.java
+++ piggyBank-module/src/main/java/com/pda/piggyBank_module/PiggyBankModuleApplication.java
@@ -2,8 +2,17 @@ package com.pda.piggyBank_module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableFeignClients
+@EnableJpaAuditing
+@ComponentScan(basePackages = {"com.pda.piggyBank_module", "com.pda.core_module"})
+
 public class PiggyBankModuleApplication {
 
 	public static void main(String[] args) {
