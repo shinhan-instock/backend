2475975418e6306dae22bb885071ba09a08dbbcb
besthong <jhjh30111@naver.com>
2025-03-05 08:17:11 +0900

    piggy bank mysql delete

--- piggyBank-module/src/main/java/com/pda/piggyBank_module/PiggyBankModuleApplication.java
+++ piggyBank-module/src/main/java/com/pda/piggyBank_module/PiggyBankModuleApplication.java
@@ -2,15 +2,17 @@ package com.pda.piggyBank_module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableScheduling
 @EnableFeignClients
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @ComponentScan(basePackages = {"com.pda.piggyBank_module", "com.pda.core_module"})
 
 public class PiggyBankModuleApplication {
