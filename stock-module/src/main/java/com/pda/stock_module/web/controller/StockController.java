559dac8836a542da4e9405485ca367eea676d20c
besthong <jhjh30111@naver.com>
2025-03-04 17:20:35 +0900

    hotfix: url패턴 api 제거

--- stock-module/src/main/java/com/pda/stock_module/web/controller/StockController.java
+++ stock-module/src/main/java/com/pda/stock_module/web/controller/StockController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/stocks")
+@RequestMapping("/stocks")
 @RequiredArgsConstructor
 public class StockController {
 
