77463fd1e1b7026fc9f588020b9529374820495d
MINO1020 <dlalsgh6772@gmail.com>
2025-03-06 16:51:51 +0900

    순위기능수정

--- stock-module/src/main/java/com/pda/stock_module/repository/RankingRepository.java
+++ stock-module/src/main/java/com/pda/stock_module/repository/RankingRepository.java
@@ -14,20 +14,13 @@ public interface RankingRepository extends JpaRepository<Ranking, Long> {
     Optional<Ranking> findByStockCode(String stockCode);
     Optional<Ranking> findByStockName(String stockName);
 
-    @Query("SELECT r.currentPrice FROM Ranking r WHERE r.stockCode = :stockCode")
-    Optional<Long> findCurrentPriceByStockCode(@Param("stockCode") String stockCode);
+//    @Query("SELECT r.currentPrice FROM Ranking r WHERE r.stockCode = :stockCode")
+//    Optional<Long> findCurrentPriceByStockCode(@Param("stockCode") String stockCode);
 
 
-    @Query("SELECT r FROM Ranking r WHERE r.fluctuationRank IS NOT NULL ORDER BY r.fluctuationRank ASC")
+    @Query("SELECT r FROM Ranking r WHERE r.fluctuationRank IS NOT NULL ORDER BY CAST(r.priceChangeRate AS double) DESC")
     List<Ranking> findTop20ByFluctuationRank();
 
-    @Query("SELECT r FROM Ranking r WHERE r.marketCapRank IS NOT NULL ORDER BY r.marketCapRank ASC")
-    List<Ranking> findTop30ByMarketCapRank();
-//
-//    @Query("SELECT r FROM Ranking r WHERE r.profitAssetIndexRank IS NOT NULL ORDER BY r.profitAssetIndexRank ASC")
-//    List<Ranking> findTop30ByProfitAssetIndexRank();
-//
-//    @Query("SELECT r FROM Ranking r WHERE r.volumeRank IS NOT NULL ORDER BY r.volumeRank ASC")
-//    List<Ranking> findTop30ByVolumeRank();
+
 
 }
